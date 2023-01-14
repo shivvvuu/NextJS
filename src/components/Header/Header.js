16353d784a5360a317bb3027cec0a96d399d67d1
shivam <jhodshivam5122@gmail.com>
2023-01-15 02:11:43 +0530

    1

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { DiCssdeck } from 'react-icons/di';
+
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+
+const Header = () =>  (
+ <Container>
+  <Div1>
+    <Link>
+    
+    </Link>
+  </Div1>
+ </Container>
+);
+
+export default Header;
